d58f094df2dbf15f22a1c796f352aeed6d94b8b3
kong6349 <644608020015@dpu.ac.th>
2022-05-30 11:57:27 +0700

    create lightswitch POP and OOP

---
+++ oop1/lightswitch_oop.py
@@ -0,0 +1,22 @@
+class LightSwitch():
+    def __init__(self) -> None:
+        self.switch_status = False
+
+    def turnon(self):
+        self.switch_status = True
+    def turnoff(self):
+        self.switch_status = False
+    def show(self):
+        print(f"status = {self.switch_status}")
+
+#สร้างวัตถุ (Object) จากแม่พิมพ์ (Class)
+switch_obj = LightSwitch()
+
+#เรียกฟังห์ชัน
+switch_obj.show()
+switch_obj.turnon()
+switch_obj.show()
+switch_obj.turnoff()
+switch_obj.show()
+switch_obj.turnoff()
+switch_obj.show()
---
+++ oop1/lightswitch_procedure.py
@@ -0,0 +1,18 @@
+#สร้างฟังก์ชันเปิด ปิดไฟ
+def turnon():
+    global switch_status
+    switch_status = True
+
+def turnoff():
+    global switch_status
+    switch_status = False
+
+switch_status = False
+
+print(f"Status = {switch_status}")
+turnon()
+print(f"Status = {switch_status}")
+turnoff()
+print(f"Status = {switch_status}")
+turnoff()
+print(f"Status = {switch_status}")
