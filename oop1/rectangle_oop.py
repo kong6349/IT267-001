e7939a4f7948ccc64c2a6c7f83bf7e8d4e8a4498
kong6349 <644608020015@dpu.ac.th>
2022-06-14 10:30:12 +0700

    rectangle

---
+++ oop1/rectangle_oop.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    def __init__(self,width,length) -> None:
+        self.width = width
+        self.length = length
+        self.area = 0
+    def get_data(self):
+        pass
+    def computer_area(self):
+        self.area = self.width * self.length
+    def print_area(self):
+        print(f"Rectangle Area : {self.area}")
+
+rec_obj = Rectangle(4.5,8)
+rec_obj.computer_area()
+rec_obj.print_area()
---
+++ oop1/rectangle_oop2.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self) -> None:
+        self.width = 0
+        self.length = 0
+        self.area = 0
+    def get_data(self):
+        self.width = float(input("Enter width: "))
+        self.length = float(input("Enter length: "))
+    def computer_area(self):
+        self.area = self.width * self.length
+    def print_area(self):
+        print(f"Rectangle Area : {self.area}")
+
+rec_obj = Rectangle()
+rec_obj.get_data()
+rec_obj.computer_area()
+rec_obj.print_area()
