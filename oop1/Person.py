dabdee844ff1d02ff8670fcc21d6974d675984ad
kong6349 <644608020015@dpu.ac.th>
2022-06-14 11:26:54 +0700

    Person

---
+++ oop1/Person.py
@@ -0,0 +1,29 @@
+from unicodedata import name
+
+
+class Person:
+    def __init__(self,name,gender,profession,study) -> None:
+        self.name = name
+        self.gender = gender
+        self.profession = profession
+        self.study = study
+
+    def work(self):
+        print(f'{self.name} is working as a {self.profession}')
+    def studie(self):
+        print(f'{self.name} studies for {self.study} hour(s)') 
+    def show(self):
+        print(f'Name: {self.name} Gender: {self.gender} Profession: {self.profession} study: {self.study} ')
+
+jessa = Person("Jessa","Male","Software Engineer",10)
+jessa.show()
+jessa.work()
+jessa.studie()
+
+jon = Person("Jon","Male","Doctor",15)
+jon.show()
+jon.work()
+jon.studie()
+
+lalisa = Person("Lalisa","Female","Korean Singer",13)
+lalisa.work()
