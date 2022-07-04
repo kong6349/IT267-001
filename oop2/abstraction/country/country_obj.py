eda8de6a0b4eb03cbfae502aad917b0f00772aeb
kong6349 <644608020015@dpu.ac.th>
2022-07-04 14:12:53 +0700

    country

---
+++ oop2/abstraction/country/country_obj.py
@@ -0,0 +1,10 @@
+from country_supclass import *
+
+eu = Europe('Norwar',5505859)
+eu.location = '5956 °N 1041 °E'
+eu.capital = 'Oslo'
+eu.show_detail()
+
+a= Asia('Malaysia',22331)
+a.gdp = 336.7
+a.show_detail()
